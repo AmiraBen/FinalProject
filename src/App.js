5c3bc829725ef02b960c2fd33a71eacf539731b1
Amira Bennani <amirabennani1955m@gmail.com>
2018-10-17 05:20:35 +0100

    Add map,search functionality and highlight selected place.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,132 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import "./App.css";
+import GoogleMap from "./components/GoogleMap.component.js";
+import ListView from "./components/ListView.component.js";
+import LocationFilter from "./components/LocationFilter.component.js";
+import Marker from "./components/Marker.component.js";
 
 class App extends Component {
+  state = {
+    center: {
+      lat: 36.6968513,
+      lng: 2.9238258
+    },
+    zoom: 15,
+    filter: "",
+    fakeData: [
+      {
+        name: "place 1",
+        id: 1,
+        lat: 36.6968513,
+        lng: 2.9238258
+      },
+      {
+        name: "place 2",
+        id: 2,
+        lat: 36.6968513,
+        lng: 2.8238258
+      },
+      {
+        name: "place 3",
+        id: 3,
+        lat: 36.705087,
+        lng: 2.8391443
+      },
+      {
+        name: "place 4",
+        id: 4,
+        lat: 36.7174111,
+        lng: 2.8395032
+      },
+      {
+        name: "place 5",
+        id: 5,
+        lat: 36.7768513,
+        lng: 2.9395032
+      },
+      {
+        name: "place 6",
+        id: 6,
+        lat: 36.7178513,
+        lng: 2.9238258
+      },
+      {
+        name: "place 7",
+        id: 7,
+        lat: 36.6968513,
+        lng: 2.9238258
+      }
+    ],
+    filteredList: [],
+    selected: null
+  };
+
+  handleChange = e => {
+    const filter = e.target.value;
+    this.setState(
+      prevState => {
+        return { ...prevState, filter };
+      },
+      () => {
+        this.setState(prevState => {
+          return {
+            filteredList: prevState.fakeData.filter(
+              item => item.name === filter
+            )
+          };
+        });
+      }
+    );
+  };
+  handleItemClick = (lat, lng, i) => {
+    console.log(lat, lng);
+    this.setState(prevState => {
+      return {
+        ...prevState,
+        center: {
+          lat: lat,
+          lng: lng
+        },
+        selected: i
+      };
+    });
+  };
   render() {
+    const sample =
+      this.state.filter && this.state.filteredList.length
+        ? this.state.filteredList
+        : [...this.state.fakeData].splice(3);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Fragment>
+        <LocationFilter handleChange={this.handleChange} />
+        <ListView
+          filteredData={sample}
+          handleItemClick={this.handleItemClick}
+        />
+        <GoogleMap
+          tabIndex="-1"
+          defaultCenter={{
+            center: {
+              lat: 36.6968513,
+              lng: 2.9238258
+            }
+          }}
+          defaultZoom={this.state.zoom}
+          center={this.state.center}
+          yesIWantToUseGoogleMapApiInternals
+        >
+          {this.state.fakeData.map((location, i) => (
+            <Marker
+              selected={this.state.selected}
+              key={i}
+              lat={location.lat}
+              lng={location.lng}
+              id={location.id}
+              name={location.name }
+            />
+          ))}
+        </GoogleMap>
+      </Fragment>
     );
   }
 }
